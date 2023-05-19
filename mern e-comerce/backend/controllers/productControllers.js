9582237514e2e9e2320df8ff6aed73e54568ecb7
aviator081081 <aviator081081@gmail.com>
2023-05-19 16:06:41 +0530

    added cretae update get delete for products

--- mern e-comerce/backend/controllers/productControllers.js
+++ mern e-comerce/backend/controllers/productControllers.js
@@ -1,3 +1,50 @@
-exports.getAllProducts = (req, res) => {
-  res.status(200).json({ message: "route is working fine" });
+const Product = require("../models/productModel");
+//create products
+exports.createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+  res.status(201).json({ success: true, product });
+};
+
+//get all products -- admin
+exports.getAllProducts = async (req, res) => {
+  const product = await Product.find();
+  res.status(200).json({ success: true, product });
+};
+
+//update products admin
+exports.updateProduct = async (req, res, next) => {
+  console.log(
+    req.params.id +
+      "----------------------------------------------------------------"
+  );
+  let product = await Product.findById(req.params.id);
+  console.log(
+    product + "----------------------------------------------------------------"
+  );
+  if (!product) {
+    return res
+      .status(500)
+      .json({ success: false, message: "product not found" });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+exports.deleteProduct = async (req, res, next) => {
+  const product = await Product.findByIdAndDelete(req.params.id);
+  if (!product) {
+    return res
+      .status(500)
+      .json({ success: false, message: "product deletion unsuccessful" });
+  }
+
+  return res.status(200).json({ success: true, product });
 };
--- mern e-comerce/backend/server.js
+++ mern e-comerce/backend/server.js
@@ -1,10 +1,10 @@
 const app = require("./app");
 
 const dotenv = require("dotenv");
-
+const connectDatabase = require("./config/database");
 //config
-dotenv.config({path:"backend/config/config.env"});
-
-app.listen(process.env.PORT,()=>{
-    console.log(`server is listening on http://localhost:${process.env.PORT}`);
-})
+dotenv.config({ path: "backend/config/config.env" });
+connectDatabase();
+app.listen(process.env.PORT, () => {
+  console.log(`server is listening on http://localhost:${process.env.PORT}`);
+});
