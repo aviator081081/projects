a5451884ddb08f57e7ecf5ab171d2507260b7186
aviator081081 <aviator081081@gmail.com>
2023-06-14 18:21:40 +0530

    add, update, delete reviews

--- mern e-comerce/backend/models/productModel.js
+++ mern e-comerce/backend/models/productModel.js
@@ -16,7 +16,7 @@ const productSchema = new mongoose.Schema({
     maxLength: [8, "price cannot excced 8 characters"],
   },
 
-  rating: {
+  ratings: {
     type: Number,
     default: 0,
   },
@@ -53,6 +53,11 @@ const productSchema = new mongoose.Schema({
 
   reviews: [
     {
+      user: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required: true,
+      },
       name: {
         type: String,
         required: true,
--- mern e-comerce/backend/routes/productRoute.js
+++ mern e-comerce/backend/routes/productRoute.js
@@ -5,6 +5,9 @@ const {
   updateProduct,
   deleteProduct,
   getProductDetails,
+  createProductReview,
+  getProductReviews,
+  deleteReview,
 } = require("../controllers/productControllers");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
@@ -32,5 +35,9 @@ router
   );
 
 router.route("/product/:id ").get(getProductDetails);
-
+router.route("/review").put(isAuthenticatedUser, createProductReview);
+router
+  .route("/reviews")
+  .get(getProductReviews)
+  .delete(isAuthenticatedUser, deleteReview);
 module.exports = router;
