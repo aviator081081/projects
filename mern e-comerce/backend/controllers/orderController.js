164f29194c8774549131ba2338e4b0adce888ce5
aviator081081 <aviator081081@gmail.com>
2023-06-16 18:29:42 +0530

    ordermodel, orderroute and order controller

--- mern e-comerce/backend/controllers/orderController.js
+++ mern e-comerce/backend/controllers/orderController.js
@@ -28,3 +28,65 @@ exports.newOrder = catchAsyncErrors(async (req, res, next) => {
   });
   res.status(201).json({ success: true, order });
 });
+
+//get single order
+
+exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+  if (!order) {
+    return next(new ErrorHandler("Order not found with this id", 404));
+  }
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+//get logged in user order
+exports.myOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find({ user: req.user._id });
+  res.status(200).json({
+    success: true,
+    orders,
+  });
+});
+
+//get all orders
+exports.getAllOrders = catchAsyncErrors(async(req,res,next)=>{
+    const orders = await Order.find();
+    let totalAmount = 0;
+    orders.forEach((order)=>{
+        totalAmount += order.totalPrice; 
+    })
+    res.status(200).json({
+        success:true,
+        totalAmount,
+        orders
+    });
+})
+
+//update order status -- admin
+
+exports.updateOrderStatus = catchAsyncErrors(async(req,res,next)=>{
+    const order = await Order.find(req.params.id);
+    if(order.orderStatus === "Delivered"){
+        return next(new ErrorHandler("you have already delivered this order",400))
+    }
+    order.orderItems.forEach(order =>{
+        await updateStock(order.Product,order.quantity);
+    })
+
+    order.orderStatus = req.body.status;
+    
+    if(req.body.status === "Delivered"){
+        order.deliverdAt = Date.now();
+    }
+
+    await order.save({validateBeforeSave:false});
+    res.status(200).json({
+        success:true
+    })
+})
--- mern e-comerce/backend/routes/orderRoute.js
+++ mern e-comerce/backend/routes/orderRoute.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const {newOrder} = require("../controllers/orderController")
-const router = express.Router(); 
+const {
+  newOrder,
+  getSingleOrder,
+  myOrders,
+} = require("../controllers/orderController");
+const router = express.Router();
 
-const {isAuthenticatedUser,authorizeRoles} = require("../middleware/auth");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
-router.route("/order/new").post(isAuthenticatedUser,newOrder);
+router.route("/order/new").post(isAuthenticatedUser, newOrder);
 
+router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
+
+router.route("/orders").get(isAuthenticatedUser, myOrders);
 module.exports = router;
